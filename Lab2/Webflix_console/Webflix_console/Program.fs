11249cfa1741ed5683ada9d81b9036477030ccad
Étienne B. Auger <etienne.bauger@gmail.com>
2015-02-17 18:44:57 -0500

    New console

---
+++ Lab2/Webflix_console/Webflix_console/Program.fs
@@ -0,0 +1,7 @@
+﻿// Obtenez des informations sur F# via http://fsharp.net
+// Voir le projet 'Didacticiel F#' pour obtenir de l'aide.
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // retourne du code de sortie entier
