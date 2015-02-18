5e05a7ee0643a5375484d6698f6913a55492894f
Étienne B. Auger <etienne.bauger@gmail.com>
2015-02-17 19:25:07 -0500

    new module WebflixModel

---
+++ Lab2/Webflix_console/WebflixModel/Script.fsx
@@ -0,0 +1,8 @@
+﻿// En savoir plus sur F# sur http://fsharp.net. Consultez le projet 'Didacticiel F#'
+// pour obtenir une aide supplémentaire sur la programmation F#.
+
+#load "Library1.fs"
+open WebflixModel
+
+// Définir votre code de script de bibliothèque ici
+
